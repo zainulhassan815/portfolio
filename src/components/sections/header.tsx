21a4750872873b447ae48c1a7adae49007a7426e
zainulhassan815 <zainulhassan4330@gmail.com>
2025-08-09 04:38:07 +0500

    Make header responsive for smaller screens.

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,13 +1,26 @@
+"use client";
+
 import { Typography } from "@/components/ui/typography";
 import { cn } from "@/lib/utils";
-import { ComponentProps, FC } from "react";
+import { ComponentProps, FC, useState } from "react";
 import { Button } from "../ui/button";
+import { Menu, X } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const Header: FC<ComponentProps<"header">> = ({ className, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={cn("h-(--header-height) w-full py-6", className)} {...props}>
       <div className="mx-auto flex items-center justify-between">
-        <Typography variant="h3">Dreamers Lab</Typography>
+        <a href="#">
+          <Typography variant="h3">Dreamers Lab</Typography>
+        </a>
         <nav className="hidden items-center gap-2 md:flex">
           <Button asChild variant="ghost">
             <a href="#projects">
@@ -30,6 +43,45 @@ export const Header: FC<ComponentProps<"header">> = ({ className, ...props }) =>
             </a>
           </Button>
         </nav>
+        <div className="md:hidden">
+          <DropdownMenu onOpenChange={setIsOpen}>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" variant="outline">
+                {isOpen ? <X /> : <Menu />}
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Button asChild variant="ghost">
+                  <a href="#projects">
+                    <Typography variant="p">Projects</Typography>
+                  </a>
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button asChild variant="ghost">
+                  <a href="#testimonials">
+                    <Typography variant="p">Testimonials</Typography>
+                  </a>
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button asChild variant="ghost">
+                  <a href="#blog">
+                    <Typography variant="p">Blog</Typography>
+                  </a>
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Button asChild variant="ghost">
+                  <a href="#contact">
+                    <Typography variant="p">Contact</Typography>
+                  </a>
+                </Button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
     </header>
   );
